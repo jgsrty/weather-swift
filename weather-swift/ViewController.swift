c5551cc7f6ec9eee181d70039ffb2289dd0221e5
RtyXmd <573850993@qq.com>
2021-06-27 15:39:48 +0800

    init

---
+++ weather-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather-swift
+//
+//  Created by 荣天阳 on 2021/6/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
